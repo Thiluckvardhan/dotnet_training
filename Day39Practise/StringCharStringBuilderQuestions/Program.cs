1398fcf9bd091bd71762686005d8ecf79ec60b2f
Thiluck Vardhan Vemula <youremail@example.com>
2026-02-13 09:09:32 +0530

    Updated Questions

--- Day39Practise/StringCharStringBuilderQuestions/Program.cs
+++ Day39Practise/StringCharStringBuilderQuestions/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using static System.Net.Mime.MediaTypeNames;
 
-namespace StringCharStringBuilder25Questions
+namespace StringCharStringBuilderQuestions
 {
     class Program
     {
