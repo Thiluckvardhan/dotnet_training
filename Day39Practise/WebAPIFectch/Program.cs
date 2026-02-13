4e1b4ea552faebda2061df2a70eaeacfbf4cd098
Thiluck Vardhan Vemula <youremail@example.com>
2026-02-13 09:09:04 +0530

    Added 10 Questions

---
+++ Day39Practise/WebAPIFectch/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace WebAPIFectch
+{
+
+    public class program
+    {
+        public static async Task Fetcher()
+        {
+            using HttpClient client=new();
+            string result= await client.GetStringAsync("https://jsonplaceholder.typicode.com/todos/");
+            System.Console.WriteLine(result);
+        }
+        public static async Task Main()
+        {
+            await Fetcher();
+        }
+    }
+}
